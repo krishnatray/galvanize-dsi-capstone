2cf460fbcaf3f0ee5f2f67e9d2ebcf8ace049cc8
Sushil Sharma <sushilsharma71@gmail.com>
2018-04-25 01:14:58 -0700

    day 2 commit

---
+++ src/train_model3.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 24 12:58:33 2018
+
+@author: SS
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 24 12:58:33 2018
+
+@author: SS
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 24 08:55:16 2018
+
+@author: SS
+"""
+
+#importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+#import nltk
+#nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+import re
+#matplotlib inline
+
+
+def clean_data(text_field):
+    text_field = re.sub('[^a-zA-Z0-9]', ' ',text_field.lower())
+    return text_field
+
+
+#importing the dataset
+file_name="labelled.csv"
+data = pd.read_csv(file_name)
+
+data['is_cardiologist'] = data.specialty.str.contains("Card")
+
+corpus = data.procedure.apply(clean_data)
+corpus.head()
+
+# split into train test
+from sklearn.model_selection import train_test_split
+y = data['is_cardiologist'].values
+X_train, X_test, y_train, y_test = train_test_split(corpus, y,test_size=0.25, random_state=0)
+
+
+# Bag of words model
+print ("TFIDF vectorize...")
+from sklearn.feature_extraction.text import TfidfVectorizer
+cv = TfidfVectorizer(stop_words= "english")
+X_train_cv = cv.fit_transform(X_train).toarray()
+X_test_cv = cv.transform(X_test).toarray()
+
+
+# Feature Scaling
+# =============================================================================
+# from sklearn.preprocessing import StandardScaler
+# sc = StandardScaler()
+# X_train_scaled = sc.fit_transform(X_train_cv)
+# X_test_scaled = sc.transform(X_test_cv)
+# =============================================================================
+
+# Fitting Naive Bayes import GaussianNB
+from sklearn.naive_bayes import MultinomialNB
+model = MultinomialNB()
+model.fit(X_train_cv, y_train)
+
+#predicting the test results
+y_pred = model.predict(X_test_cv)
+y_pred_proba = model.predict_proba(X_test_cv)
+
+#Making the confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+tn, fp, fn, tp = cm.ravel()
+
+# classification report
+from sklearn.metrics import classification_report, accuracy_score
+print(classification_report(y_test, y_pred))
+print("Accuracy Score Train:", accuracy_score(y_train, model.predict(X_train_cv), normalize = True))
+print("Accuracy Score Test:", accuracy_score(y_test, y_pred, normalize = True))
+
+# ROC curve
+from sklearn.metrics import roc_curve
+fpr, tpr, ths = roc_curve(y_test, y_pred_proba[:,1])
+plt.plot(fpr,tpr)
+plt.show()
+
+from sklearn.metrics import auc
+roc_auc = auc(fpr,tpr)
+print("Area under the curve : ", roc_auc)
+
+# save model
+from sklearn.externals import joblib
+joblib.dump(model, "model.joblib")
+
+# load model
+# from sklearn.externals import joblib
+#pulled_model = joblib.load("model.joblib")
+#y_pulled_pred = pulled_model.predict(X_test)
+#y_pulled_pred_proba = pulled_model.predict_proba(X_test)
+
+
